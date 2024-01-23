c3a05254de77a9df8de4c042343fec870d8bac6a
LorenzoGarzelli <lorenzo.garzelli2908@gmail.com>
2024-01-23 17:26:33 +0100

    refactored  redisMessageStore

--- backend/utils/redisMessageStore.ts
+++ backend/utils/redisMessageStore.ts
@@ -1,26 +1,18 @@
 import dotenv from 'dotenv';
 import { createClient } from 'redis';
-
-// import { createClient } from '';
-// import { createClient } from 'redis';
-import { RedisClientType } from '@redis/client';
-import * as p from '@redis/client';
-import { Message, MessageAck } from '../types/types';
-import { Socket } from 'socket.io';
+import { ClientSocket, Message, MessageAck } from '../types/types';
 
 dotenv.config({ path: './config.env' });
 
 class RedisMessageStore {
   private client: ReturnType<typeof createClient>;
 
-  constructor(_client?: RedisClientType) {
-    this.client =
-      //_client ||
-      createClient({
-        url: process.env.REDIS_DB_URL,
-        username: process.env.REDIS_USERNAME,
-        password: process.env.REDIS_PASSWORD,
-      });
+  constructor() {
+    this.client = createClient({
+      url: process.env.REDIS_DB_URL,
+      username: process.env.REDIS_USERNAME,
+      password: process.env.REDIS_PASSWORD,
+    });
   }
 
   async connect() {
@@ -31,13 +23,14 @@ class RedisMessageStore {
   }
 
   async saveMessage(message: Message) {
-    await this.connect();
+    if (!this.client.isOpen) await this.client.connect();
 
-    //const value = JSON.stringify(message);
+    const recipientMessagesBox = await this.client.exists(
+      `message:${message.to}`
+    );
 
-    const exist = await this.client.exists(`message:${message.to}`);
-
-    if (exist < 1) await this.client.json.set(`message:${message.to}`, '$', []);
+    if (recipientMessagesBox < 1)
+      await this.client.json.set(`message:${message.to}`, '$', []); // create the messages box if doesn't exist
 
     this.client
       .multi()
@@ -47,90 +40,54 @@ class RedisMessageStore {
         Number(process.env.CONVERSATION_TTL) || 86400
       )
       .exec();
-
-    /*this.client
-      .multi()
-      .rPush(`message:${message.to}`, value)
-      .expire(
-        `message:${message.to}`,
-        Number(process.env.CONVERSATION_TTL) || 86400
-      )
-      .exec();*/
   }
 
   async findMessagesForUser(userRoomId: string) {
-    await this.connect();
+    if (!this.client.isOpen) await this.client.connect();
 
-    //const results = await this.client.lRange(`message:${userRoomId}`, 0, -1);
-    //return results.map(result => JSON.parse(result));
-
-    //@ts-ignore
-    const results = await this.client.json.get(`message:${userRoomId}`, 0, -1);
-    return results;
+    const foundMessages = await this.client.json.get(
+      //@ts-ignore
+      `message:${userRoomId}`,
+      0,
+      -1
+    );
+    return foundMessages;
   }
   async findAcksForUser(userRoomId: string) {
-    await this.connect();
+    if (!this.client.isOpen) await this.client.connect();
     //@ts-ignore
-    const results = await this.client.json.get(`ack:${userRoomId}`);
+    const foundAcks = await this.client.json.get(`ack:${userRoomId}`);
 
-    return results;
+    return foundAcks;
   }
 
   async deleteAckForUser(userRoomId: string, uuid: string) {
-    await this.connect();
+    if (!this.client.isOpen) await this.client.connect();
 
-    this.client.json.DEL(`ack:${userRoomId}`, `$.[?(@.uuid=="${uuid}")]`);
+    const query = `$.[?(@.uuid=="${uuid}")]`;
+    this.client.json.DEL(`ack:${userRoomId}`, query);
   }
 
-  async deleteMessages(socket: Socket, message: MessageAck) {
-    //TODO JSON.GET ack:816ccbc9-e281-40f8-a189-c82b79bedc5f "$.[?(@.uuid==\"b6211243-39b4-483e-8367-cb848a42e85a747474723\")]"
+  async saveAckAndDeleteMessage(socket: ClientSocket, message: MessageAck) {
+    if (!this.client.isOpen) await this.client.connect();
 
-    const exist = await this.client.exists(`ack:${message.from}`);
+    const recipientAcksBox = await this.client.exists(`ack:${message.to}`);
 
-    if (exist < 1) await this.client.json.set(`ack:${message.from}`, '$', []);
+    if (recipientAcksBox < 1)
+      await this.client.json.set(`ack:${message.to}`, '$', []);
 
-    this.client.json
+    await this.client.json.arrAppend(`ack:${message.to}`, '$', {
+      uuid: message.uuid,
+      //@ts-ignore
+      from: socket.roomId,
+      status: message.status,
+    });
 
-      .arrAppend(`ack:${message.from}`, '$', {
-        uuid: message.uuid,
-        //@ts-ignore
-        to: socket.roomId,
-        status: message.status,
-      })
-
-      .then(() => {
-        //? send ack to sender user
-        socket.to(message.from).emit('messages ack', {
-          uuid: message.uuid,
-          //@ts-ignore
-          to: socket.roomId,
-          status: message.status,
-        });
-
-        this.client.json.DEL(
-          //@ts-ignore
-          `message:${socket.roomId}`,
-          `$.[?(@.uuid=="${message.uuid}")]`
-        );
-      });
-
-    /*this.client
-        .multi()
-        .rPush(
-          `ack:${message.from}`,
-          JSON.stringify({
-            uuid: message.uuid,
-            to: message.to,
-            status: message.status,
-          })
-        )
-        .expire(
-          `message:${message.to}`,
-          Number(process.env.CONVERSATION_TTL) || 86400
-        )
-        .exec();
-
-      this.client.lRem(`message:${socketId}`, 1, JSON.stringify(message));*/
+    await this.client.json.DEL(
+      //@ts-ignore
+      `message:${socket.roomId}`,
+      `$.[?(@.uuid=="${message.uuid}")]`
+    );
   }
 }
 export default new RedisMessageStore();
