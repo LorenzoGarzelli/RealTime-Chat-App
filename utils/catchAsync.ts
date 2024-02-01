2016da00dea46f1f4d12b8b70cb638dd4f48d338
LorenzoGarzelli <lorenzo.garzelli2908@gmail.com>
2024-02-01 16:44:16 +0100

    remove old files and directories

--- utils/catchAsync.ts
+++
@@ -1,8 +0,0 @@
-import { NextFunction, Request, Response } from 'express';
-
-//TODO Add correct type
-module.exports = (fn: any) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    fn(req, res, next).catch(next);
-  };
-};
